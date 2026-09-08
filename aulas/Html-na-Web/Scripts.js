c7a5a0734c6ed9e9df82a73dcdb639689698a940
otavio <otaviocarmo21@gmail.com>
2026-09-08 10:48:14 -0300

    melhorando algumas estapas e concluindo o butão de remover

--- aulas/Html-na-Web/Scripts.js
+++ aulas/Html-na-Web/Scripts.js
@@ -1,4 +1,5 @@
-const task = []
+let tasks = []
+
 
 //
 const taskDescription = document.getElementById("task-description")
@@ -17,28 +18,49 @@ function addTasck() {
         checked: false
     }
 
-    task.push(newTask)
-    console.log("Task: ", task)
+    tasks.push(newTask)
     renderList()
 }
 
 function renderList () {
 
     taskList.innerText = ""
-    task.forEach(task => {
+    tasks.forEach(task => {
         const li = document.createElement("li")
 
         const checkbox = document.createElement("input")
         checkbox.type = "checkbox"
         checkbox.checked = task.checked
+
+        checkbox.addEventListener("change", function() {
+            task.checked = checkbox.checked
+
+            if(task.checked) {
+                text.classList.add("completed")
+
+            } else {
+                text.classList.remove("completed")
+            }
+        })
+
         li.appendChild(checkbox)
 
         const text = document.createElement("span")
         text.textContent = task.description
+
+        if(task.checked) {
+                text.classList.add("completed")
+
+        }
         li.appendChild(text)
 
         const button = document.createElement("button")
         button.textContent = "Remover"
+
+        button.addEventListener("click", function() {
+            tasks = tasks.filter(t => t != task)
+            renderList()
+        })
         li.appendChild(button)
 
         taskList.appendChild(li)
